7b726ef92d9581526a0f988003b3a3a809124631
ram-9047 <ramg786@live.com>
2022-09-18 12:22:36 +0530

    adds delete function

---
+++ models/product.js
@@ -0,0 +1,31 @@
+const db = require("../util/database");
+const Cart = require("./cart");
+
+module.exports = class Product {
+  constructor(id, title, price, description, imageUrl) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  save() {
+    return db.execute(
+      "INSERT INTO products (title,price,description,imageUrl) VALUES (?, ?, ?, ?)",
+      [this.title, this.price, this.description, this.imageUrl]
+    );
+  }
+
+  static deleteById(id) {
+    return db.execute("DELETE FROM products WHERE products.id = ?", [id]);
+  }
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
+  }
+
+  static findById(id) {
+    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
+  }
+};
